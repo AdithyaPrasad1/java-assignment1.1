f13775dc98efc6d8d25eff5b4ca10a750fbecaba
AdithyaPrasad <hadithya71@gmail.com>
2023-06-09 17:11:47 +0530

    first commit

---
+++ grade.js
@@ -0,0 +1,11 @@
+let sub1=70;
+let sub2=80;
+let sub3=90;
+let sub4=50;
+let totalmark=(sub1+sub2+sub3+sub4);
+console.log("total mark=",totalmark);
+if (totalmark>=250){
+    console.log("PASSED")
+}else{
+    console.log("FAILED")
+}
